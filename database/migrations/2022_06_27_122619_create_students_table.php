e53c494985762e1f189603e807baeee356bc1a10
Reika Akuzawa <l06981@googlemail.com>
2022-06-27 15:30:19 +0200

    create database tables

---
+++ database/migrations/2022_06_27_122619_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->date('birthday')->nullable();
+            $table->integer('tel');
+            $table->string('email', 100);
+            $table->integer('course_id')->unsigned();
+            $table->date('start_date');
+            $table->timestamps();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ database/migrations/2022_06_27_131336_create_lesson_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessonTokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('payment_id')->unsigned();
+            $table->boolean('isUsed');
+            $table->timestamps();
+
+            $table->foreign('payment_id')->references('id')->on('payments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessonTokens');
+    }
+};
