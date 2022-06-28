82237c88139e8f0fa9e3a3466900df94e1756a66
Reika Akuzawa <l06981@googlemail.com>
2022-06-28 22:45:18 +0200

    Create Index page #1: show dummy data

---
+++ app/Models/Lesson.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the student that owns the Lesson
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class, 'foreign_key', 'other_key');
+    }
+}
