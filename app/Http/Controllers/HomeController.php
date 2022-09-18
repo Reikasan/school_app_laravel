5613e253df3ea5220884f431f204e794b58e81ba
Reika Akuzawa <l06981@googlemail.com>
2022-09-18 16:01:05 +0200

    change route index from homeController to Lesson

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,43 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $lessons = [
-            (object) [
-                'id' => 1,
-                'student_id' => (object) [
-                    'id' => 1,
-                    'name' => 'user_1',
-                ],
-                'date' => '2022-06-11',
-                'start_time' => '10:30',
-                'lesson_token_id' => 2,
-                'created_at' => now(),
-            ],
-            (object) [
-                'id' => 2,
-                'student_id' => (object) [
-                    'id' => 3,
-                    'name' => 'user_3',
-                ],
-                'date' => '2022-06-11',
-                'start_time' => '12:30',
-                'lesson_token_id' => 5,
-                'created_at' => now(),
-            ],
-            (object) [
-                'id' => 3,
-                'student_id' => (object) [
-                    'id' => 1,
-                    'name' => 'user_1',
-                ],
-                'date' => '2022-06-15',
-                'start_time' => '10:30',
-                'lesson_token_id' => 10,
-                'created_at' => now(),
-            ],
-        ];
-
-        // $lessons = Lesson::all()->sortByAsc('date');
-        return view('home', ['lessons' => $lessons]);
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,8 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 // Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\LessonController::class, 'index'])->name('index');
+// Route::get('/#today', [App\Http\Controllers\LessonController::class, 'showTodaysLessons'])->name('todaysLesson');
+// Route::get('/#tomorrow', [App\Http\Controllers\LessonController::class, 'showTomorrowsLessons'])->name('tomorrowsLesson');
+// Route::get('/#twoMoreDay', [App\Http\Controllers\LessonController::class, 'show+2daysLessons'])->name('plus2daysLesson');
+Route::get('/menu/', [App\Http\Controllers\HomeController::class, 'index'])->name('menu');
